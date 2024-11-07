22b83dfc6f9d0985a6817a62930dd94cf71f14cf
David-MacRo <david.macroberts@outlook.com>
2024-11-06 20:10:16 -0800

    Implemented spring library
    https://start.spring.io/
    Also added a few classes.

---
+++ src/test/java/com/David_MacRo/WeatherForecastApp/WeatherForecastAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.David_MacRo.WeatherForecastApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
